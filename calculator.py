8945f2144d9ad1807db13e51af8b8da762ff110a
Adam Scheck <32208770+adambryce12@users.noreply.github.com>
2025-11-12 15:01:14 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
+def add(a, b): a + b
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def sub(a, b): a - b
 
+def mul(a, b): a * b
 
+def div(a, b): b / a # raise ZeroDivisionError if a == 0
+
+def log(a, b): loga(b)# use math library + raise ValueError
+
+def exp(a, b): ab
