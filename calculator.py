7fc399e96d8f39c559f7c32519ac6fb7bb3e6fd5
aidensoriano67 <dickthatass69@gmail.com>
2025-11-18 16:53:44 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,23 +4,31 @@ import math
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1:
-        raise ValueError("Invalid base for logarithm")
+        raise ValueError("Invalid base")
     if b <= 0:
-        raise ValueError("Logarithm undefined for non-positive numbers")
+        raise ValueError("Invalid number")
     return math.log(b, a)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
+
+def hypotenuse(a, b):
+    return math.sqrt(a*a + b*b)
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Negative number")
+    return math.sqrt(a)
